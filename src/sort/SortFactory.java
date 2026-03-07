11585a4cb9e52896f13101842f16b7c631c60811
Dsgalvist <Diegogalvis682@gmail.com>
2026-03-06 18:18:58 -0700

    Implemented SortFactory and added package documentation

--- src/sort/SortFactory.java
+++ src/sort/SortFactory.java
@@ -1,5 +1,39 @@
+/**
+ * @Author Diego Galvis
+ *
+ * SortFactory is used to return the correct sorting algorithm
+ * based on the option chosen by the user.
+ */
+
 package sort;
 
 public class SortFactory {
 
+    @SuppressWarnings("unchecked")
+    public static <T> SortStrategy<T> getSortStrategy(char type) {
+
+        switch (Character.toLowerCase(type)) {
+
+            case 'b':
+                return (SortStrategy<T>) new BubbleSort<>();
+
+            case 's':
+                return (SortStrategy<T>) new SelectionSort<>();
+
+            case 'i':
+                return (SortStrategy<T>) new InsertionSort<>();
+
+            case 'm':
+                return (SortStrategy<T>) new MergeSort<>();
+
+            case 'q':
+                return (SortStrategy<T>) new QuickSortMedian3<>();
+
+            case 'z':
+                return (SortStrategy<T>) new HeapSort<>();
+
+            default:
+                throw new IllegalArgumentException("Invalid sorting option.");
+        }
+    }
 }
--- src/sort/package-info.java
+++ src/sort/package-info.java
@@ -1,1 +1,8 @@
+/**
+ * @Author Diego Galvis
+ *
+ * This package contains the sorting strategy interface and the factory
+ * class used to choose a sorting algorithm in the program.
+ */
+
 package sort;
